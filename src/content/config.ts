c86ed75cbf710d9ebc34b97eb3db9ab7a86fae89
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-03-07 01:16:29 +0100

    try with md files

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 // 1. Import utilities from `astro:content`
-import {z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
-  type: 'data', // v2.5.0 and later
+  type: "content", // v2.5.0 and later
   schema: z.object({
-    name: z.string()
+    name: z.string(),
   }),
- });
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'games': blogCollection,
+  games: blogCollection,
 };
