433e10bc4ed4bc730570cf61dde3d702ee4e46d7
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-03-09 18:02:59 +0100

    static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
@@ -7,6 +7,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: "server",
-  adapter: netlify()
+  output: "static",
 });
